9bbfcdc37e07f7b17fc13e5c3016d29b8fdc88d6
Ivan Lepesii <yukinoyuu@Ivans-MacBook-Air.local>
2024-03-16 22:54:20 -0500

    server: noStore fix, to remove chaching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react"
+import { unstable_noStore as noStore } from "next/cache"
 import fetchTest from "@/server/fetchTest"
 import testValidator from "@/server/zfetchTest"
 import { z } from "zod"
@@ -8,6 +9,7 @@ const schema = testValidator
 type result = z.infer<typeof schema>
 
 export default async function Home() {
+    noStore()
     return(
         <>
             <h1>Hello from Postgres:</h1>
