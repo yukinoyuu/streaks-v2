c2555d032ced0d6d8f50bb5d292c889f910dbd12
Ivan Lepesii <yukinoyuu@Ivans-MacBook-Air.local>
2024-03-16 22:45:04 -0500

    to be fixed: zod validator

--- src/server/fetchTest.ts
+++ src/server/fetchTest.ts
@@ -3,5 +3,7 @@ import testValidator from "./zfetchTest";
 
 export default async function fetchTest() {
     const obj = await sql`SELECT * FROM test`
+    // console.log(obj)
+    // testValidator.parse(obj) not working for some reason
     return obj
 }
